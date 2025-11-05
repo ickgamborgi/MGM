e049722373806d00fc1447813a89c9a18f6e86c2
Henrique Gamborgi <henriquegamborgi@gmail.com>
2025-11-05 19:12:14 -0300

    Update Links and Animations

--- index.php
+++ index.php
@@ -16,15 +16,15 @@ require_once('includes/connect.php');
     <!-- Open Graph Meta Tags -->
     <meta property="og:title" content="Matos & Gamborgi Menezes Advocacia">
     <meta property="og:description" content="O escritório Matos & Gamborgi Menezes Advocacia oferece soluções jurídicas em Direito Empresarial, Imobiliário, Civil, Trabalhista e Eleitoral. Atuação ética, transparente e eficiente em Lages e Florianópolis. Agende sua consulta.">
-    <meta property="og:image" content="">
-    <meta property="og:url" content="">
+    <meta property="og:image" content="https://advocaciamgm.com.br/images/intro.webp">
+    <meta property="og:url" content="https://advocaciamgm.com.br">
     <meta property="og:type" content="website">
 
     <!-- Twitter Meta Tags -->
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:title" content="Matos & Gamborgi Menezes Advocacia">
     <meta name="twitter:description" content="O escritório Matos & Gamborgi Menezes Advocacia oferece soluções jurídicas em Direito Empresarial, Imobiliário, Civil, Trabalhista e Eleitoral. Atuação ética, transparente e eficiente em Lages e Florianópolis. Agende sua consulta.">
-    <meta name="twitter:image" content="">
+    <meta name="twitter:image" content="https://advocaciamgm.com.br/images/intro.webp">
 
     <!-- Ubuntu Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
@@ -61,7 +61,7 @@ require_once('includes/connect.php');
                     <h3 class="hidden">Navegação Principal</h3>
 
                     <div class="logo-header col-start-1">
-                        <a href="index.html"><img src="./images/logo-min-white.svg" alt="MGM Minified Logo"></a>
+                        <a href="index.php"><img src="./images/logo-min-white.svg" alt="MGM Minified Logo"></a>
                     </div>
 
                     <button id="burger-button"></button>
@@ -69,10 +69,10 @@ require_once('includes/connect.php');
                     <div class="links-header">
                         <h4 class="hidden">Links Cabeçalho</h4>
                         <ul>
-                            <li><a href="index.html" class="nav-item"><h5>Início</h5></a></li>
-                            <li><a href="index.html#areas" class="nav-item"><h5>Áreas</h5></a></li>
-                            <li><a href="index.html#profissionais" class="nav-item"><h5>Profissionais</h5></a></li>
-                            <li><a href="index.html#contato" class="nav-item"><h5>Contato</h5></a></li>
+                            <li><a href="index.php" class="nav-item"><h5>Início</h5></a></li>
+                            <li><a href="index.php#areas" class="nav-item"><h5>Áreas</h5></a></li>
+                            <li><a href="index.php#profissionais" class="nav-item"><h5>Profissionais</h5></a></li>
+                            <li><a href="index.php#contato" class="nav-item"><h5>Contato</h5></a></li>
                         </ul>
                     </div>
                 </nav>
@@ -96,7 +96,7 @@ require_once('includes/connect.php');
                 <h3 class="hidden">Título Introdutório</h3>
                 <div class="introduction-title-con">
                     <h4 class="introduction-subtitle">Sobre Nós</h4>
-                    <h5 class="introduction-title">Profissionalismo e Confiança</h4>
+                    <h5 class="introduction-title">Profissionalismo e Confiança</h5>
                 </div>
                 <div class="introduction-body-con">
                     <h4 class="hidden">Texto Introdutório</h4>
@@ -104,7 +104,7 @@ require_once('includes/connect.php');
                     <p class="introduction-body-text">Nossa missão é gerar resultados concretos, oferecendo soluções jurídicas que atendam às necessidades de cada cliente, sempre explicando de forma clara e objetiva o caminho jurídico proposto.</p>
                     <p class="introduction-body-text">Nos guiamos pelos princípios de competência e especialização, oferecendo estratégias jurídicas eficazes; ética e transparência, garantindo integridade e clareza em todas as orientações; e agilidade e proatividade, antecipando riscos e assegurando a melhor proteção jurídica para nossos clientes.</p>
                 </div>
-                <a class="button" href="#">
+                <a class="button" href="https://api.whatsapp.com/send?phone=5548999368405&text=Ol%C3%A1,%20encontrei%20o%20n%C3%BAmero%20via%20website%20e%20preciso%20de%20ajuda%20legal!" target=_blank>
                     <h4>Agendar Consulta ></h4>
                 </a>
             </div>
@@ -120,7 +120,7 @@ require_once('includes/connect.php');
             <div class="areas-cards-con col-span-full">
                 <h3 class="hidden">Cards de Áreas de Atuação</h3>
                 <!-- Civil -->
-                <a class="areas-card" href="#">
+                <a class="areas-card" href="https://api.whatsapp.com/send?phone=5548999368405&text=Ol%C3%A1,%20encontrei%20o%20n%C3%BAmero%20via%20website%20e%20preciso%20de%20ajuda%20com%20Direito%20Civil!" target=_blank>
                     <img class="areas-card-image" src="./images/icon_civil.svg" alt="Direito Civil">
                     <div class="areas-card-title-con">
                         <h4 class="areas-card-subtitle">Direito</h4>
@@ -128,7 +128,7 @@ require_once('includes/connect.php');
                     </div>
                 </a>
                 <!-- Empresarial -->
-                <a class="areas-card" href="#">
+                <a class="areas-card" href="https://api.whatsapp.com/send?phone=5548999368405&text=Ol%C3%A1,%20encontrei%20o%20n%C3%BAmero%20via%20website%20e%20preciso%20de%20ajuda%20com%20Direito%20Empresarial!" target=_blank>
                     <img class="areas-card-image" src="./images/icon_empresarial.svg" alt="Direito Empresarial">
                     <div class="areas-card-title-con">
                         <h4 class="areas-card-subtitle">Direito</h4>
@@ -136,7 +136,7 @@ require_once('includes/connect.php');
                     </div>
                 </a>
                 <!-- Imobiliário -->
-                <a class="areas-card" href="#">
+                <a class="areas-card" href="https://api.whatsapp.com/send?phone=5548999368405&text=Ol%C3%A1,%20encontrei%20o%20n%C3%BAmero%20via%20website%20e%20preciso%20de%20ajuda%20com%20Direito%20Imobili%C3%A1rio!" target=_blank>
                     <img class="areas-card-image" src="./images/icon_imobiliario.svg" alt="Direito Imobiliário">
                     <div class="areas-card-title-con">
                         <h4 class="areas-card-subtitle">Direito</h4>
@@ -144,7 +144,7 @@ require_once('includes/connect.php');
                     </div>
                 </a>
                 <!-- Administrativo -->
-                <a class="areas-card" href="#">
+                <a class="areas-card" href="https://api.whatsapp.com/send?phone=5548999368405&text=Ol%C3%A1,%20encontrei%20o%20n%C3%BAmero%20via%20website%20e%20preciso%20de%20ajuda%20com%20Direito%20Administrativo!" target=_blank>
                     <img class="areas-card-image" src="./images/icon_administrativo.svg" alt="Direito Administrativo">
                     <div class="areas-card-title-con">
                         <h4 class="areas-card-subtitle">Direito</h4>
@@ -152,7 +152,7 @@ require_once('includes/connect.php');
                     </div>
                 </a>
                 <!-- Trabalhista -->
-                <a class="areas-card" href="#">
+                <a class="areas-card" href="https://api.whatsapp.com/send?phone=5548999368405&text=Ol%C3%A1,%20encontrei%20o%20n%C3%BAmero%20via%20website%20e%20preciso%20de%20ajuda%20com%20Direito%20Trabalhista!" target=_blank>
                     <img class="areas-card-image" src="./images/icon_trabalhista.svg" alt="Direito Trabalhista">
                     <div class="areas-card-title-con">
                         <h4 class="areas-card-subtitle">Direito</h4>
@@ -160,7 +160,7 @@ require_once('includes/connect.php');
                     </div>
                 </a>
                 <!-- Eleitoral -->
-                <a class="areas-card" href="#">
+                <a class="areas-card" href="https://api.whatsapp.com/send?phone=5548999368405&text=Ol%C3%A1,%20encontrei%20o%20n%C3%BAmero%20via%20website%20e%20preciso%20de%20ajuda%20com%20Direito%20Eleitoral!" target=_blank>
                     <img class="areas-card-image" src="./images/icon_eleitoral.svg" alt="Direito Eleitoral">
                     <div class="areas-card-title-con">
                         <h4 class="areas-card-subtitle">Direito</h4>
@@ -180,7 +180,7 @@ require_once('includes/connect.php');
             <div class="team-cards-con col-span-full">
                 <h3 class="hidden">Cards de Profissionais</h3>
                 <!-- Leonardo Matos -->
-                <a class="team-member" id="leo" href="#">
+                <a class="team-member" id="leo" href="https://www.linkedin.com/in/leonardomluz/" target=_blank>
                     <div class="team-image">
                       <img src="./images/leonardo-matos.webp" alt="Leonardo Matos da Luz">
                       <div class="team-image-overlay"></div>
@@ -201,7 +201,7 @@ require_once('includes/connect.php');
                     </div>
                 </a>
                 <!-- Matheus Gamborgi -->
-                <a class="team-member" id="matt" href="#">
+                <a class="team-member" id="matt" href="https://www.linkedin.com/in/matheus-gamborgi-menezes-345016144/">
                     <div class="team-image">
                       <img src="./images/matheus-gamborgi.webp" alt="Matheus Gamborgi Menezes">
                       <div class="team-image-overlay"></div>
@@ -235,7 +235,7 @@ require_once('includes/connect.php');
             <div class="office-cards-con col-span-full">
                 <h3 class="hidden">Cards de Escritórios</h3>
                 <!-- Lages -->
-                <a class="office-card" href="#" id="lages">
+                <a class="office-card" href="https://www.google.com/maps/search/Av.+Beliz%C3%A1rio+Ramos+n%C2%BA+3735,+Ed.+Number+One,+Sagrado+Cora%C3%A7%C3%A3o+De+Jesus/"  target=_blank id="lages">
                     <div class="office-card-con">
                         <h4 class="office-city">Lages</h4>
                         <h4 class="office-state">Santa Catarina</h4>
@@ -244,7 +244,7 @@ require_once('includes/connect.php');
                     <img class="office-icon" src="./images/icon-loc.svg" alt="Ícone Localização">
                 </a>
                 <!-- Florianópolis -->
-                <a class="office-card" href="#" id="florianopolis">
+                <a class="office-card" href="https://maps.app.goo.gl/bdgpeojFyu9TRTc96" target=_blank id="florianopolis">
                     <div class="office-card-con">
                         <h4 class="office-city">Florianópolis</h4>
                         <h4 class="office-state">Santa Catarina</h4>
@@ -262,7 +262,7 @@ require_once('includes/connect.php');
                 <h3 class="office-subtitle">Contato</h3>
                 <h4 class="office-title">Como podemos ajudar?</h4>
             </div>
-                <p class="contact-text">Ao enviar este formulário, você demonstra estar ciente e concorda com nossa <a href="#"><span>Política de Privacidade</span></a></p>
+                <p class="contact-text">Ao enviar este formulário, você demonstra estar ciente e concorda com nossa <a href="privacidade.php" target=_blank><span>Política de Privacidade</span></a></p>
             </div>
             <div class="contact-form-con col-span-full ">
                 <h3 class="hidden">Formulário de Contato</h3>
@@ -280,7 +280,7 @@ require_once('includes/connect.php');
                     <input type="number" id="math-answer" name="math_answer" placeholder="1 + 1 = ?">
                     <input type="hidden" id="math-expected" name="math_expected">
                     
-                    <button name="submit" type="submit" value="Send"><span>Enviar ></span></button>
+                    <button class="button" name="submit" type="submit" value="Send"><span>Enviar ></span></button>
                 </form>
 
                 <div id="feedback" class="col-span-full"><p>Por favor, preencha todos os campos</p></div>
@@ -294,15 +294,17 @@ require_once('includes/connect.php');
         <h2 class="hidden">Rodapé</h2>
         <div class="footer-con col-span-full">
             <div class="footer-1">
-                <a href="index.html"><img src="./images/logo-footer.svg" alt="MGM Minified Logo"></a>
+                <a href="index.php"><img src="./images/logo-footer.svg" alt="MGM Minified Logo"></a>
                 <div class="footer-nav">
-                    <a href="index.html">Página Principal</a>
-                    <a href="index.html#contato">Contato</a>
-                    <a href="#">Política de Privacidade</a>
+                    <a href="index.php">Página Principal</a>
+                    <a href="index.php#contato">Contato</a>
+                    <a href="privacidade.php" target=_blank>Política de Privacidade</a>
                 </div>
             </div>
             <div class="footer-2">
-                <img src="./images/icon-whats.svg" alt="Ícone WhatsApp">
+                <a href="https://api.whatsapp.com/send?phone=5548999368405&text=Ol%C3%A1,%20encontrei%20o%20n%C3%BAmero%20via%20website%20e%20preciso%20de%20ajuda%20legal!" target=_blank>
+                    <img src="./images/icon-whats.svg" alt="Ícone WhatsApp">
+                </a>
                 <p>2025 &copy; Matos & Gamborgi Menezes <br>Todos os Direitos Reservados</p>
             </div>
         </div>
